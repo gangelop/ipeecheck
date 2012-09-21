64fc36cdc8ad6204e76217417588f1971e685843
James Lott <jlott024@gmail.com>
2012-09-21 02:08:32 -0700

    Removed forked branding

--- ipeecheck.sh
+++ ipeecheck.sh
@@ -44,14 +44,14 @@ else
 
     echo -e "Your ip address for host $HOSTNAME has changed.\n\
 Your new ip address is:\n$CURRENT_ADDR"\
-| mailx -v -s "[IPSWITCHEE] Your ip address has changed" "$EMAIL"
+| mailx -v -s "[IPEECHECK] Your ip address has changed" "$EMAIL"
     if $ENABLE_DESKTOP_NOTIFICATION;
       then
 	if $REMOTE_DESKTOP_NOTIFICATION;
 	  then
-	    ssh $REMOTE_NOTIFICATON_IP "DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send \"IpSwitchee\" \"Your new public IP is:\n$CURRENT_ADDR\""
+	    ssh $REMOTE_NOTIFICATON_IP "DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send \"ipeecheck\" \"Your new public IP is:\n$CURRENT_ADDR\""
 	  else
-	    DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send "IpSwitchee" "Your new public IP is:\n$CURRENT_ADDR"
+	    DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send "ipeecheck" "Your new public IP is:\n$CURRENT_ADDR"
 	fi
     fi
 fi
