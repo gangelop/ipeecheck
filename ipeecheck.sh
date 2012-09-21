d084761b42cf60ec534957a3c87ae369add6b295
James Lott <jlott024@gmail.com>
2012-09-21 02:05:30 -0700

    Removed forked branding

---
+++ ipeecheck.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#exits if there is an unbound variable or an error
+set -o nounset
+set -o errexit
+
+SERVER="http://icanhazip.com"
+ADDR_HST_FILE="$HOME/.ipeecheck_address_history"
+EMAIL=name@host.com
+ENABLE_DESKTOP_NOTIFICATION=$(true)
+REMOTE_DESKTOP_NOTIFICATION=$(false)
+REMOTE_NOTIFICATON_IP=192.168.1.X
+DESKTOP_NOTIFICATION_DISPLAY=:0
+
+#checks if address history file exists.
+#if not, it creates it.
+if [ -f "$ADDR_HST_FILE" ]
+then
+    echo "Address history file exists."
+else
+    echo "Address history file doesn't exist."
+    echo "Creating file "$ADDR_HST_FILE""
+    touch "$ADDR_HST_FILE"
+fi
+
+#gets the current addr from the specified server and
+#gets the latest addr from the address history file.
+#stores both addresses in variables
+echo "Checking current ip address..."
+CURRENT_ADDR=$(wget "$SERVER" -O - -q)
+PREVIOUS_ADDR=$(tail -n 1 "$ADDR_HST_FILE")
+
+#checks if address has changed since previous execution
+#if it hasn't changed, it does nothing.
+#else, it appends it to the history file and sends an e-mail
+if [ "$CURRENT_ADDR" = "$PREVIOUS_ADDR"  ]
+then
+    echo "Your ip address hasn't changed."
+    exit 0
+else
+    echo "Your ip address has changed."
+    echo "Storing new ip address in $ADDR_HST_FILE"
+    echo "$CURRENT_ADDR" >> "$ADDR_HST_FILE"
+
+    echo -e "Your ip address for host $HOSTNAME has changed.\n\
+Your new ip address is:\n$CURRENT_ADDR"\
+| mailx -v -s "[IPSWITCHEE] Your ip address has changed" "$EMAIL"
+    if $ENABLE_DESKTOP_NOTIFICATION;
+      then
+	if $REMOTE_DESKTOP_NOTIFICATION;
+	  then
+	    ssh $REMOTE_NOTIFICATON_IP "DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send \"IpSwitchee\" \"Your new public IP is:\n$CURRENT_ADDR\""
+	  else
+	    DISPLAY=$DESKTOP_NOTIFICATION_DISPLAY notify-send "IpSwitchee" "Your new public IP is:\n$CURRENT_ADDR"
+	fi
+    fi
+fi
