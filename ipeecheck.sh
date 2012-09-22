abc6b267fc472aa368691746d70c16ab2595ac0d
George Angelopuolos <l4than.d3vers@gmail.com>
2012-09-22 15:07:24 +0300

    added a comment

--- ipeecheck.sh
+++ ipeecheck.sh
@@ -45,6 +45,7 @@ else
     echo -e "Your ip address for host $HOSTNAME has changed.\n\
 Your new ip address is:\n$CURRENT_ADDR"\
 | mailx -v -s "[IPEECHECK] Your ip address has changed" "$EMAIL"
+#Sends desktop notifications if enabled
     if $ENABLE_DESKTOP_NOTIFICATION
       then
         if $REMOTE_DESKTOP_NOTIFICATION
