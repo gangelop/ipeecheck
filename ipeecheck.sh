bc0703fdabbb6c9ddc4fc6ef412c3e225c1f2329
lathan <lathan@somewhere.com>
2012-09-23 15:29:57 +0300

    added a blank line for readability

--- ipeecheck.sh
+++ ipeecheck.sh
@@ -45,6 +45,7 @@ else
     echo -e "Your ip address for host $HOSTNAME has changed.\n\
 Your new ip address is:\n$CURRENT_ADDR"\
 | mailx -v -s "[IPEECHECK] Your ip address has changed" "$EMAIL"
+
 #Sends desktop notifications if enabled
     if $LOCAL_DESKTOP_NOTIFICATION
     then
